98d0e447f26581a939a82d2a16ee874923fe3185
Brian Anderson <stormtrooperguy@Brians-MBP.home>
2016-09-11 00:09:04 -0400

    still tweaking

--- bb8/bb8.ino
+++ bb8/bb8.ino
@@ -6,12 +6,12 @@
 #include <Adafruit_VS1053.h>
 #include <SD.h>
 
-// These are the pins used for the music maker shield
+// These are the pins used for the music maker shield. Pasted straight from bundled sample code. 
 #define SHIELD_RESET  -1      // VS1053 reset pin (unused!)
 #define SHIELD_CS     7      // VS1053 chip select pin (output)
 #define SHIELD_DCS    6      // VS1053 Data/command select pin (output)
-#define CARDCS 4     // Card chip select pin
-#define DREQ 3       // VS1053 Data request, ideally an Interrupt pin
+#define CARDCS 4             // Card chip select pin
+#define DREQ 3               // VS1053 Data request, ideally an Interrupt pin
 
 Adafruit_VS1053_FilePlayer musicPlayer = Adafruit_VS1053_FilePlayer(SHIELD_RESET, SHIELD_CS, SHIELD_DCS, DREQ, CARDCS);
 
@@ -20,53 +20,47 @@ void setup() {
   // speaker volume
   int volume = 10;
 
-  Serial.begin(9600);
+  //Serial.begin(9600);
 
   // make our random more random
   randomSeed(analogRead(0));
 
   // initialise the music player
   if (! musicPlayer.begin()) { // initialise the music player
-    Serial.println(F("Couldn't find VS1053, do you have the right pins defined?"));
+    //Serial.println(F("Couldn't find VS1053, do you have the right pins defined?"));
     while (1);
   }
 
   if (!SD.begin(CARDCS)) {
-    Serial.println(F("SD failed, or not present"));
+    //Serial.println(F("SD failed, or not present"));
     while (1);  // don't do anything more
   }
 
   // Set volume for left, right channels. lower numbers == louder volume!
   musicPlayer.setVolume(volume, volume);
 
-  // Set all of our musicPlayer GPIO pins to output.
-
-  for (int thisPin = 1; thisPin <= 7; thisPin++) {
-    musicPlayer.GPIO_pinMode(thisPin, OUTPUT);
-  }
-
+  // Set all the pins we are going to use to output.
   for (int thisPin = 22; thisPin <= 55; thisPin++) {
     pinMode(thisPin, OUTPUT);
   }
 
-  /***** Two interrupt options! *******/
-  // This option uses timer0, this means timer1 & t2 are not required
-  // (so you can use 'em for Servos, etc) BUT millis() can lose time
-  // since we're hitchhiking on top of the millis() tracker
-  //musicPlayer.useInterrupt(VS1053_FILEPLAYER_TIMER0_INT);
-
-  // This option uses a pin interrupt. No timers required! But DREQ
-  // must be on an interrupt pin. For Uno/Duemilanove/Diecimilla
-  // that's Digital #2 or #3
-  // See http://arduino.cc/en/Reference/attachInterrupt for other pins
-  // *** This method is preferred
+  // From sample code. 
   if (! musicPlayer.useInterrupt(VS1053_FILEPLAYER_PIN_INT))
     Serial.println(F("DREQ pin is not an interrupt pin"));
 }
 
 void loop() {
 
-  // 26 Holo randomly on off
+  // Pin mapping:
+  //
+  // 22      = Vocodor
+  // 24      = Eye
+  // 26      = Holo
+  // 28      = Blue logic
+  // 30      = White steady logic
+  // 32 - 40 = White random logic
+
+  // 26 Holo may be off or on each loop. 
 
   int holoOn = random(0,100);
 
@@ -79,13 +73,21 @@ void loop() {
     digitalWrite(26, LOW);
   }
 
-  // turn off the vocoder at the start of the loop. Pause a bit. Then go. 
+  // 24 Eye always on
 
-  digitalWrite(22, LOW);
+  digitalWrite(24, HIGH);
+
+  // 28 Blue logic, always on
+
+  digitalWrite(28, HIGH);
+
+  //30 upper white logic, always on
+
+  digitalWrite(30, HIGH);
 
   // This is a super dirty way to get a shuffled playlist.
   // The files are all named nnn.mp3. We just pick a random number
-  // between 1 and 138 and play that file.
+  // between 1 and 27 and play that file.
   //
   // The conversion between strings and chars was based on googling
   // and may not actually be good.
@@ -111,50 +113,35 @@ void loop() {
 
   while (musicPlayer.playingMusic) {
 
-    // 22      = Vocodor
-    // 24      = Eye
-    // 26      = Holo
-    // 28      = Blue logic
-    // 30      = White steady logic
-    // 32 - 40 = White random logic
-
     // Pin 22 is the vocoder. Ultimately this should pulse
-    // to the music, but one step at a time. For now flickering is OK.
+    // to the souns, but one step at a time. For now flickering is OK.
 
     digitalWrite(22, HIGH);
-    delay(random(2, 4));
+    delay(random(1, 4));
     digitalWrite(22, LOW);
-
-    // 24 Eye always on
-
-    digitalWrite(24, HIGH);
-
-    // 28 Blue logic, always on
-
-    digitalWrite(28, HIGH);
-
-    //30 White logic, always on
-
-    digitalWrite(30, HIGH);
 
     // 32 - 40, random white logic. 
 
-   //int logicPins[] = {32, 34, 36, 38, 40};
-
-   //int thisPin = random(0,4);
-
-   //digitalWrite(thisPin, HIGH);
-   //delay(random(50, 100));
-   //digitalWrite(thisPin, LOW);
-
-
     int thisPin = random(32, 41);
-    int thisDelay = random(5, 10);
+    int thisDelay = random(1, 8);
 
     digitalWrite(thisPin, HIGH);
     delay(thisDelay);
     digitalWrite(thisPin, LOW);
 
   }
+
+  // at the end of the track, turn off mouth and holo. turn lower logic on solid.
+
+  digitalWrite(22, LOW);
+  digitalWrite(26, LOW);
+
+  for (int thisPin = 32; thisPin <= 41; thisPin++) {
+    digitalWrite(thisPin, HIGH);
+  }
+
+  // pause a bit, then do it again!
+
+  delay(random(1000, 3000));
 
 }
